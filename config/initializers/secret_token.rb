20d23bc00a5fd5290e6014cefdfaa41474d14579
jameskyokushin <james@kyokushin.com.ph>
2012-01-28 10:29:34 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fusionzion::Application.config.secret_token = '17a84821505a73b738eefc95704e79aeb2061f077a0bda9e81dff80d6292cde8b9ad357d2ffcf33bd8115c62903eaa5823b511ff5f1bebbd02a88cf0a47ca68b'
